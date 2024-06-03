2fa9c8bdcf92eb76e1e7844c0d34b5f0892b89ba
Bekmyrza Tursyn <122518618+Bek-arch@users.noreply.github.com>
2024-06-04 01:33:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,31 +6,25 @@ from PIL import Image
 import numpy as np
 from util import visualize, set_background
 
-# Установка фона
-set_background('./bg.png')
+set_background('./5c6dc1b5b81bcfd4c5da9b3948c2e879-1.jpg')
 
-# Установка заголовка и подзаголовка
 st.title('Обнаружение опухолей на МРТ мозга')
 st.header('Пожалуйста, загрузите изображение')
 
-# Загрузка файла
 uploaded_file = st.file_uploader('Загрузите изображение в формате PNG, JPG или JPEG', type=['png', 'jpg', 'jpeg'])
 
-# Настройка модели
 config = get_cfg()
 config_path = model_zoo.get_config_file('COCO-Detection/retinanet_R_101_FPN_3x.yaml')
 config.merge_from_file(config_path)
 config.MODEL.WEIGHTS = './model/model.pth'
-config.MODEL.DEVICE = 'cpu'  # Использование CPU
+config.MODEL.DEVICE = 'cpu'  
 
 predictor = DefaultPredictor(config)
 
-# Обработка загруженного изображения
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
     image_np = np.array(image)
 
-    # Обнаружение объектов
     prediction_outputs = predictor(image_np)
 
     detection_threshold = 0.5
@@ -40,5 +34,4 @@ if uploaded_file is not None:
             x1, y1, x2, y2 = [int(coord) for coord in box.tolist()]
             detected_boxes.append([x1, y1, x2, y2])
 
-    # Визуализация результатов
     visualize(image, detected_boxes)
